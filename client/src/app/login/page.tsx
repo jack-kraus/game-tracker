352f3aa5811f7a618156560aa968d7a8248f9437
Jack Kraus <jackkraus3856@gmail.com>
2024-06-09 15:25:00 -0400

    Feat: Get User Posts
    On the profile page, show just that user's posts

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -14,7 +14,7 @@ export default function Login() {
 
     const form_content = state === FormState.Login ? <LoginForm/> : <SignupForm/>;
     return <>
-        <div className="m-0">
+        <div>
             <div className="pl-3">
                 <button className={`primary-button rounded-b-none ${state !== FormState.Login ? "bg-scale-1000 text-primary" : ""}`} onClick={() => setState(FormState.Login)}>Login</button>
                 <button className={`primary-button rounded-b-none ${state !== FormState.SignUp ? "bg-scale-1000 text-primary" : ""}`} onClick={() => setState(FormState.SignUp)}>Sign-Up</button>
--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -1,19 +1,26 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import { logout } from "../../data/actions";
+import Post from '@/components/Post';
 
 export default async function Profile() {
     const supabase = createClient();
     const { data, error } = await supabase.auth.getUser();
+    let posts;
 
     if (error || !data?.user) {
         redirect('/login');
+    }
+    else {
+        posts = await supabase.from("post").select("*").eq("author", data.user.id);
+        posts = posts.data;
     }
 
     return <>
         <h1 className="text-scale-0 underline">Hello {data.user.email}</h1>
         <form>
             <button formAction={logout} className='bg-primary'>Log-Out</button>
+            {posts ? posts.map((item, index:number) => <Post key={index} {...{...item, author:"Steve"}}/>) : <p>Pending...</p>}
         </form>
     </>;
 }
