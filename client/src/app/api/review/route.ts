798226f7856746c7787843c599a04571363b3b36
Jack Kraus <jackkraus3856@gmail.com>
2024-06-30 17:33:50 -0400

    Feat: Infinite Scrolling with Button

--- client/src/app/api/review/route.ts
+++ client/src/app/api/review/route.ts
@@ -4,23 +4,37 @@ import { createClient } from "@/utils/supabase/server";
 import moment from 'moment';
 import Yup from 'yup';
 
+const PER_PAGE = 2;
+
 export async function GET(request : NextRequest) {
     // get search params
-    let filter = request.nextUrl.searchParams.get("filter");
-    
+    let filter = request.nextUrl.searchParams.get("filter"),
+    page = request.nextUrl.searchParams.get("page");
+    const page_num = parseInt(page) || 0;
+
     // get client
     const supabase = createClient();
+
+    // get review count
+    let review_count : number;
+    try{
+        const { count, error } = await supabase
+            .from(filter === "following" ? "post_user_like_following" : "post_user_like")
+            .select('*', { count: 'exact', head: true });
+        if (!count || error ) return Response.json({success: false, error:`Server Error`});
+        review_count = count;
+    } catch (error : any) { return Response.json({success: false, error:`${error}`}); }
 
     // get reviews table
     let reviews;
     try{
         const { data, error } = await supabase
-            .from("post_user_like")
+            .from(filter === "following" ? "post_user_like_following" : "post_user_like")
             .select('*')
-            .range(0,10);
+            .range(page_num*PER_PAGE,(page_num+1)*PER_PAGE-1);
         if (!data || error ) return Response.json({success: false, error:`Server Error`, object:error});
         reviews = data;
-    } catch (error : any) { return Response.json({success: false, error:`1: ${error}`}); }
+    } catch (error : any) { return Response.json({success: false, error:`${error}`}); }
 
     // edit date info
     reviews.map((review) => review.created_at = moment(review.created_at).format('MM/DD/YYYY h:mm a'));
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,22 +1,46 @@
 "use client";
 import Post from '@/components/items/Post';
 import LoadingHandler from '@/components/ui/LoadingHandler';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 
 export default function Posts() {
-    let query = useQuery({
-        queryKey: ['repoData'],
-        queryFn: () =>
-          fetch(`/api/review`).then((res) =>
-            res.json(),
-          ),
+    const fetchReviews = async ({ pageParam }) => {
+      const res = await fetch('/api/review?page=' + pageParam)
+      return res.json();
+    }
+    let query = useInfiniteQuery({
+      queryKey: ['reviews'],
+      queryFn: fetchReviews,
+      initialPageParam: 0,
+      getNextPageParam: (lastPage, _allPages, lastPageParam) => {
+        if (lastPage.data.length === 0) { return undefined; }
+        return lastPageParam + 1;
+      },
     });
-    let { data } = query;
+    let { data,
+      error,
+      fetchNextPage,
+      hasNextPage,
+      isFetching,
+      isFetchingNextPage,
+      status } = query;
+
+    let posts = data && data.pages ? data.pages.map((item) => item.data).flat() : undefined;
 
     return  <>
       <h1 className="text-scale-0 underline">Feed</h1>
-      <LoadingHandler {...query}>
-        {data ? data.data.map((item : any, index:number) => <Post key={index} {...item}/>) : <></>}
-      </LoadingHandler>
+      {posts ? posts.map((item : any, index:number) => <Post key={index} {...item}/>) : <></>}
+      <button
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetchingNextPage}
+        >
+          {isFetchingNextPage
+            ? 'Loading more...'
+            : hasNextPage
+              ? 'Load More'
+              : 'Nothing more to load'}
+        </button>
     </>;
 }
+
+/* */
