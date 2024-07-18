e679382257f9f2bc905fde2a82eb5e59816c6e03
Jack Kraus <jackkraus3856@gmail.com>
2024-07-18 00:30:32 -0400

    Fix: Formatting Fix mobile post

--- client/src/components/items/Post.tsx
+++ client/src/components/items/Post.tsx
@@ -33,16 +33,16 @@ export default function Post({ id, title, game, game_title, game_cover, content,
     const { session } = useUser();
 
     return (
-        <article className="box-item gap-3">
+        <article className="box-item gap-2 sm:gap-3">
             {type !== "game" && <a className="sm:w-32 w-16 shrink-0 group text-center" href={`/game/${game}`}>
                 <img className="object-contain w-full rounded-md group-hover:brightness-150" src={game_cover} alt={game_title + " cover"}/>
                 <cite className="group-link-item break-words hyphens-auto text-sm text-end">{game_title}</cite>
             </a>}
             <div className="h-full w-full inline-flex flex-col justify-start sm:gap-2 gap-1.5">
-                <a href={`/review/${id}`} className="link-item self-start"><h1>{title}</h1></a>
-                <div className="flex gap-1">
+                <a href={`/review/${id}`} className="link-item self-start break-words"><h1>{title}</h1></a>
+                <div className="flex gap-1 flex-wrap">
                     {type !== "user" && <><p className="text-scale-300">by</p>
-                    <a href={`/user/${author}`} className="group w-auto inline-flex gap-1 items-center self-start">
+                    <a href={`/user/${author}`} className="group w-auto inline-flex gap-1 items-center self-start flex-wrap">
                         <FaUser size={20} className="p-1  bg-scale-500 text-scale-0 rounded-full group-hover:rounded-3xl object-contain group-hover:bg-scale-0 group-hover:text-scale-500"/>
                         <p className="group-link-item break-words">{username}</p>
                     </a></>}
