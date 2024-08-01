9701d42654c362791882c914acf927818900c2aa
Jack Kraus <jackkraus3856@gmail.com>
2024-08-01 16:29:06 -0400

    Feat: Edit Profile

--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -5,6 +5,8 @@ import { redirect } from "next/navigation";
 import { Metadata } from "next";
 import { FaUser } from "react-icons/fa";
 import FollowerTable from "@/components/ui/FollowerTable";
+import { RiSettings2Fill } from "react-icons/ri";
+import { cn } from "@/data/utils";
 
 export const metadata: Metadata = {
     title: "Profile | Leveler",
@@ -21,6 +23,7 @@ export default async function Profile() {
 
     return <>
         <div className="box-item flex-col gap-3 items-center max-w-xs">
+            <a className={cn("hover-button", "self-end")} href="/profile/edit"><RiSettings2Fill/></a>
             <h1 className="text-scale-0 underline">Hello, {data?.username}</h1>
             <FaUser className="border-opacity-25 transition-all bg-scale-500 p-3 rounded-full flex justify-center items-center" size={80} color="white"/>
             <FollowerTable {...data}/>
--- client/src/components/form/LoginForm.tsx
+++ client/src/components/form/LoginForm.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { login, signup } from "@/data/actions"
+import { change_password, change_username, login, signup } from "@/data/actions"
 import { useForm, FormProvider } from "react-hook-form";
 import hook from "@/data/hook_options";
 import Input from "./Input";
@@ -93,6 +93,75 @@ export function SignupForm() {
             />
             <div className="flex w-full items-center gap-3">
                 <button type="submit" disabled={loading} className='primary-button grow'>Sign-Up</button> {loading ? <ThreeDots color="white" width={20} height={20}/> : <></>}
+            </div>
+        </form>
+    </FormProvider>
+}
+
+export function ChangePasswordForm() {
+    const methods = useForm();
+    const { watch, setError, handleSubmit } = methods;
+    const [loading, setLoading] = useState(false);
+
+    async function onSubmit(data : any) {
+        setLoading(true);
+        const response = await change_password(data);
+        if (response) {
+            const { key, message } : any = response;
+            setError(key, {type:"server", message: message});
+        }
+        setLoading(false);
+    }
+
+    return <FormProvider {...methods}>
+        <form className='flex flex-col gap-3 w-full' noValidate onSubmit={handleSubmit(onSubmit)}>
+            <Input
+                label="Password"
+                id="password"
+                type="password"
+                placeholder="Password"
+                hookOptions={hook.password_validation("Password")}
+            />
+            <Input
+                label="Confirm Password"
+                id="confirm_password"
+                type="password"
+                placeholder="Password"
+                hookOptions={hook.confirm_password_validation("Confirm Password", watch, "password")}
+            />
+            <div className="flex w-full items-center gap-3">
+                <button type="submit" disabled={loading} className='primary-button grow'>Change Password</button> {loading ? <ThreeDots color="white" width={20} height={20}/> : <></>}
+            </div>
+        </form>
+    </FormProvider>
+}
+
+export function ChangeUsernameForm({defaultUsername} : {defaultUsername? : string}) {
+    const methods = useForm({ defaultValues: { username: defaultUsername } });
+    const { setError, handleSubmit } = methods;
+    const [loading, setLoading] = useState(false);
+
+    async function onSubmit(data : any) {
+        setLoading(true);
+        const response = await change_username(data);
+        if (response) {
+            const { key, message } : any = response;
+            setError(key, {type:"server", message: message});
+        }
+        setLoading(false);
+    }
+
+    return <FormProvider {...methods}>
+        <form className='flex flex-col gap-3 w-full' noValidate onSubmit={handleSubmit(onSubmit)}>
+            <Input
+                label="Username"
+                id="username"
+                type="text"
+                placeholder="Username"
+                hookOptions={hook.username_validation("Username")}
+            />
+            <div className="flex w-full items-center gap-3">
+                <button type="submit" disabled={loading} className='primary-button grow'>Change Username</button> {loading ? <ThreeDots color="white" width={20} height={20}/> : <></>}
             </div>
         </form>
     </FormProvider>
